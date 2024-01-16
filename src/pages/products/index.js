6ed1edc80f2d03ce09edb0b35b592c5aab737c48
jiamudza <jiamudza@gmail.com>
2024-01-16 20:32:31 +0700

    modified:   src/pages/products/index.js

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -2,12 +2,6 @@ import React from 'react'
 import LogoutButton from '../components/logoutButton'
 
 function Products() {
-    const logOutAction = (e) => {
-        e.preventDefault() 
-        
-        localStorage.removeItem("@Login")
-        localStorage.removeItem("userData")
-    }
   return (
     <>
         
